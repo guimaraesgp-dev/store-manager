107a7b041a57616ecb95f0c391f76a988ca5244b
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2023-05-04 00:27:05 -0300

    REQ 4

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { productsController } = require('../controllers');
+const validateProducts = require('../middlewares/productsValidation');
 
 const router = express.Router();
 
 router.get('/', productsController.getAllProducts);
 router.get('/:id', productsController.getProductsId);
-router.post('/', productsController.createProducts);
+router.post('/', validateProducts, productsController.createProducts);
 
 module.exports = router;
